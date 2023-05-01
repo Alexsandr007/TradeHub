8a3c69944715df6ba0c2315e3e3116d9b053c91b
Alexandr007 <aleksone_01@mail.ru>
2023-05-01 19:42:53 +0300

    add database and connecting media files

---
+++ products/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+
+class Features(models.Model):
+    name = models.CharField(max_length=50)
+    value = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'features'
+
+
+CONDITION_CHOICES = (
+    ('Новое', 'new'),
+    ('Б/у', 'old')
+)
+
+
+CATEGORY_CHOICES = (
+    ('Недвижимость', 'Real estate'),
+    ('Авто и транспорт', 'Car and transport'),
+    ('Ремонт и стройка', 'Renovation and construction'),
+    ('Хобби, спорт и туризм', 'Hobbies, sports and tourism'),
+    ('Все для детей и родителей', 'All for children and parents'),
+    ('Мебель', 'Furniture'),
+    ('Женский гардероб', 'Womens wardrobe'),
+    ('Животные', 'Animals'),
+    ('Все для дома', 'All for your home'),
+    ('Телефоны и планшеты', 'Phones and tablets'),
+    ('Сад и огород', 'Garden and vegetable garden'),
+    ('Электроника', 'Electronics'),
+    ('Компьютерная техника', 'Computer hardware'),
+    ('Бытовая техника', 'Household appliances'),
+    ('Мужской гардероб', 'Mens wardrobe'),
+    ('Услуги', 'Services'),
+    ('Готовый бизнес и оборудование', 'Ready-made business and equipment'),
+    ('Красота и здоровье', 'Beauty and health'),
+    ('Работа', 'Job'),
+    ('Свадьбы и праздники', 'Weddings and celebrations'),
+    ('Прочее', 'Other'),
+)
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=50)
+    cost = models.FloatField()
+    сondition = models.ChoiceField(choices=CONDITION_CHOICES)
+    date_created = models.DateTimeField(auto_now_add=True)
+    short_description = models.CharField(max_length=50)
+    features = models.ForeignKey(Features, on_delete=models.CASCADE)
+    description = models.TextField()
+    location = models.CharField(max_length=50)
+    category = models.ChoiceField(choices=CATEGORY_CHOICES)
+
+    class Meta:
+        db_table = 'products'
---
+++ users/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+import TradeHub.products.models as p
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, UserManager)
+from django.contrib.auth.validators import UnicodeUsernameValidator
+
+
+SEX_CHOICES = (
+    ('Мужчина', 'man'),
+    ('Женщина', 'women')
+)
+
+
+SELLER_CHOICES = (
+    ('Частное лицо', 'private_person'),
+    ('Компания', 'company')
+)
+
+
+def user_directory_path(instance, filename):
+    # путь, куда будет осуществлена загрузка MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        "username",
+        max_length=20,
+        unique=True,
+        help_text="Required. 20 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        validators=[username_validator],
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+    )
+    email = models.EmailField(max_length=50, null=True)
+    number = models.CharField(max_length=20, null=True)
+    sex = models.ChoiceField(choices=SEX_CHOICES)
+    img = models.ImageField(upload_to=user_directory_path, height_field=100, width_field=100)
+    date_of_birth = models.DateTimeField(null=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    products = models.ForeignKey(p.Products, on_delete=models.CASCADE)
+    seller = models.ChoiceField(choices=SELLER_CHOICES)
+    exchange = models.BooleanField(default=False)
+    objects = UserManager()
+
+    EMAIL_FIELD = False
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        db_table = 'users'
+
+
+class Reviews(models.Model):
+    user_whom = products = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user_who = products = models.ForeignKey(Users, on_delete=models.CASCADE)
+    value = models.IntegerField()
+
+    class Meta:
+        db_table = 'reviews'
